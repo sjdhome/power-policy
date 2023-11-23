afd220dbacf3b24fc9a425a9df554bb673626c3f
sjdhome <sjdhoome@gmail.com>
2023-11-23 15:56:13 +0800

    Add setting for EPP

--- performance.sh
+++ performance.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 echo performance | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
-echo 0 | tee /sys/devices/system/cpu/cpu*/power/energy_perf_bias
+echo 0 | tee /sys/devices/system/cpu/cpu*/power/energy_perf_bias  # Intel only
+echo performance | tee /sys/devices/system/cpu/cpu0/cpufreq/energy_performance_preference
+
--- powersave.sh
+++ powersave.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 echo powersave | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
-echo 15 | tee /sys/devices/system/cpu/cpu*/power/energy_perf_bias
+echo 15 | tee /sys/devices/system/cpu/cpu*/power/energy_perf_bias  # Intel only
+echo power | tee /sys/devices/system/cpu/cpu0/cpufreq/energy_performance_preference
+
